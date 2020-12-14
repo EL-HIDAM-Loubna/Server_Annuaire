566f2a78d499317c029d453ec4e13b6bf8fb28a5
EL HIDAM Loubna <loubna.elhidam@gmail.com>
2020-12-14 14:05:50 +0100

    All Project

---
+++ ServiceAnnuaire/src/main/java/aiac/project/ServiceAnnuaireApplication.java
@@ -0,0 +1,13 @@
+package aiac.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceAnnuaireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceAnnuaireApplication.class, args);
+	}
+
+}
---
+++ ServiceAnnuaire/src/test/java/aiac/project/demo/ServiceAnnuaireApplicationTests.java
@@ -0,0 +1,13 @@
+package aiac.project.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceAnnuaireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
